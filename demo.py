38cfafe7099125e1d4549e6d6de0577fffc31a4f
Mohsin2686 <mohsinshafique2686@gmail.com>
2025-04-22 09:56:56 +0500

    exception module added

--- demo.py
+++ demo.py
@@ -1,9 +1,22 @@
 from customer_churn_prediction.logger import get_logger
+from customer_churn_prediction.exception import CustomerChurnException
+import sys
 
 # Initialize the logger
 logger = get_logger(__name__)
 print(__name__)
 
 # Test the logger
-logger.info("This is a test log message.")
-logger.error("This is a test error message.")
+logger.info("Welcome to the customer churn prediction project")
+logger.debug("This is a debug message")
+logger.error("This is an error message")
+logger.warning("This is a warning message")
+logger.critical("This is a critical message")
+
+
+# Test the exception handling
+try:
+    raise ValueError("This is a test error")
+except ValueError as e:
+    logger.error("An error occurred: %s", str(e))
+    raise CustomerChurnException(e, sys) from e
