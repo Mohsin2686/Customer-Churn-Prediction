74b41d0990c864238c70308a1a8a9490df57a2fc
Mohsin2686 <mohsinshafique2686@gmail.com>
2025-05-20 12:09:26 +0500

    model training module added

--- customer_churn_prediction/pipline/training_pipeline.py
+++ customer_churn_prediction/pipline/training_pipeline.py
@@ -4,21 +4,25 @@ from customer_churn_prediction.logger import logging
 from customer_churn_prediction.components.data_ingestion import DataIngestion
 from customer_churn_prediction.components.data_validation import DataValidation
 from customer_churn_prediction.components.data_transformation import DataTransformation
+from customer_churn_prediction.components.model_trainer import ModelTrainer
 
 
 from customer_churn_prediction.entity.config_entity import (DataIngestionConfig,
                                                             DataValidationConfig,
-                                                            DataTransformationConfig)
+                                                            DataTransformationConfig,
+                                                            ModelTrainerConfig)
 
 from customer_churn_prediction.entity.artifact_entity import (DataIngestionArtifact,
                                                               DataValidationArtifact,
-                                                              DataTransformationArtifact)
+                                                              DataTransformationArtifact,
+                                                              ModelTrainerArtifact)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     
@@ -76,6 +80,20 @@ class TrainPipeline:
             return data_transformation_artifact
         except Exception as e:
             raise CustomerChurnException(e, sys)
+        
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise CustomerChurnException(e, sys)
 
     def run_pipeline(self, ) -> None:
         """
@@ -86,6 +104,7 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(
                 data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
 
         except Exception as e:
             raise CustomerChurnException(e, sys)
